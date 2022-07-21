d9d901dd17043eab4726ea6f682d7e2fae7217aa
wxd56987 <672514143@qq.com>
2022-07-21 15:26:22 +0800

    add some module

---
+++ pages/web3.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/index.module.scss'
+import { Rodemap } from '../utils/types'
+import { useState } from 'react';
+import AdmetaSdk from 'admeta-sdk'
+
+const Web3: NextPage<Rodemap> = ({ v }) => {
+  const [account, setAccount] = useState('')
+  const handleOpenPlokadot = async () => {
+    if (account) {
+      return;
+    }
+    if (typeof window !== "undefined") {
+      const { web3Accounts, web3Enable } = await import('@polkadot/extension-dapp')
+      await web3Enable('AdMeta');
+      const allAccounts = await web3Accounts() as any[];
+      setAccount(allAccounts[0].address)
+      console.log(allAccounts)
+      new AdmetaSdk(allAccounts[0].address).init()
+    }
+  }
+
+  const formatAddress = (address: string): string => {
+    const str_1 = address.substring(0, 4)
+    const str_2 = address.substring(address.length - 4)
+    return `${str_1}......${str_2}`
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Web3</title>
+        <meta name="description" content="web3 ecology" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.web3}>
+        <div className={styles.brand}>
+          <div className={styles.title}>Trustworthy</div>
+          <div className={styles.title}>Innovative</div>
+          <div className={styles.title}>Humanistic</div>
+          <div
+            className={styles.btn}
+            onClick={handleOpenPlokadot}
+          >
+            <p>{account ? formatAddress(account) : 'Connect with Polkadot'}</p>
+          </div>
+        </div>
+        <div className={styles.circle}></div>
+      </div>
+
+      <div className={styles.footer}>
+        Powered by Web3 Ecology
+      </div>
+    </div>
+  )
+}
+export default Web3
--- utils/tools.ts
+++ utils/tools.ts
@@ -37,7 +37,7 @@ enum ApiEnum {
 }
 
 export const ApiStart = (env: string): string => {
-  return  'http://localhost:3000'
+  return  'http://localhost:3333'
 }
 
 export const formatBikeDaydate = (data: RoadmapItemType[], folder: string): RoadmapItemType[] => {
